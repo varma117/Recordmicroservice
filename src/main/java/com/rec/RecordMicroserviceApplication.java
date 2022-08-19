a4ee85bbda4666b1a8b6edde23549dcdfa578d09
unknown <cogjava3103@iiht.tech>
2022-08-19 13:28:00 +0530

    friday commit

--- src/main/java/com/rec/RecordMicroserviceApplication.java
+++ src/main/java/com/rec/RecordMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.rec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RecordMicroserviceApplication {
 
 	public static void main(String[] args) {
